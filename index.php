a609c5017534a5cc3c9f32f3305f9a952f1772ce
Nate Dillon <natedillon@gmail.com>
2013-03-08 19:26:23 -0600

    initialize the repository

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Chevmo Ranch</title>
+
+	<style type="text/css">
+	
+		body {
+			margin: 0;
+			padding: 0 20px 20px 20px;
+			background-color: #f5f5f5;
+			font-family: Arial, Helvetica, sans-serif;
+			font-size: small;
+		}
+		
+		#page {
+			display: block;
+			width: 400px;
+			margin: 0 auto;
+			padding: 0;
+		}
+		
+		#logo {
+			margin: 0;
+			padding: 0;
+			text-align: center;
+		}
+		
+		#save-the-date {
+			margin: 0;
+			padding: 20px;
+			background-color: white;
+			border: 1px solid #ddd;
+			text-align: center;
+		}
+		
+		#save-the-date a img {
+			border: none;
+		}
+		
+	</style>
+
+</head>
+
+<body>
+
+	<div id="page">
+	
+		<h1 id="logo"><img src="images/logo.png" width="400" height="400" alt="Chevmo Ranch" /></h1>
+		
+		<p id="save-the-date"><a href="http://wedding.chevmoranch.com/"><img src="images/save-the-date.jpg" width="360" height="280" alt="Lindsey Dillon and Michael Baxter are getting married April 26, 2008!" /></a></p>
+		
+	</div> <!-- end #page -->
+
+</body>
+
+</html>
